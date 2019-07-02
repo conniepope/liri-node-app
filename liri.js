cc40e84049cf94c2ef653d40cbb815d3b273050d
conniepope <conniepope77@gmail.com>
2019-07-02 12:52:03 -0400

    cleaned up code

--- liri.js
+++ liri.js
@@ -11,10 +11,15 @@ var fs = require("fs")
 
 var moment = require("moment")
 
+// _____________________________________________________
+
 var type = process.argv[2]
 console.log(type)
+
 var inputParameter = process.argv.slice(3).join(" ");
 console.log(inputParameter)
+
+// _____________________________________________________
 
 var switchType = function(type, inputParameter) {
 
@@ -36,22 +41,20 @@ var switchType = function(type, inputParameter) {
 switchType(type, inputParameter);
 
 // _____________________________________________________
+
 function concert() {      
     
     var artist = inputParameter;
 
     var queryURLconcert = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
 
-    // console.log(queryURLconcert)
     axios.get(queryURLconcert).then(function(response) {
-        // console.log(response.data[0])
 
         for (var i = 0; i < 10; i++) {
             var concert = response.data[i];
             var format = moment.HTML5_FMT.DATETIME_LOCAL_SECONDS;
             var date = moment(concert.datetime).format("LL")
 
-            // console.log(concert)
             console.log()
             console.log("Venue: " + concert.venue.name); 
             console.log("Location: " + concert.venue.city + " " + concert.venue.region); 
@@ -59,7 +62,7 @@ function concert() {
         }
     })
     .catch(function(error) {
-          console.log("Error", error.message);
+          console.log("Sorry, can not find any concerts for this person at this time.");
     });
 }   
 // _____________________________________________________
@@ -72,38 +75,27 @@ function song() {
         song = "The Sign"
     }
 
-    // if (song = "") {
-    //     spotify.search({type: "track", query: "The Sign"})
-    //         .then(function(response){
-
-    //         })
-    // }
-    
-    // var queryURLspotify = "https://www.npmjs.com/package/node-spotify-api "         
-
     spotify                                   
         .search({
             type: "track",
             query: song})
-            .then(function(response) {
-                
-                // console.log(response)
-                for (var i = 0; i < response.tracks.items.length; i++) {
-                    var info = response.tracks.items[i]
-                    // console.log(info)
-                    console.log();  
-                    console.log(info.artists[0].name)      
-                    console.log(info.name);  
-                    console.log(info.preview_url);   
-                    console.log(info.album.name);  
-                }
-            })
-            .catch(function() {
-                song = "The+Sign";
-                console.log(song)
-             })
-}       
+        .then(function(response) {
+            
+            for (var i = 0; i < response.tracks.items.length; i++) {
+                var info = response.tracks.items[i]
 
+                console.log();  
+                console.log(info.artists[0].name)      
+                console.log(info.name);  
+                console.log(info.preview_url);   
+                console.log(info.album.name);  
+            }
+        })
+        .catch(function() {
+            song = "The+Sign";
+            console.log(song)
+            })
+}       
 // _____________________________________________________
 
 function movie() {      
@@ -131,7 +123,7 @@ function movie() {
         console.log("Actors: " + movie.Actors)  
         console.log();  
 
-})
+    })
 } 
 // _____________________________________________________
 
@@ -140,19 +132,18 @@ function says() {
     fs.readFile("random.txt", "utf8", function(err, data) {
         if (err) {
             return console.log(err);
-          }
-          else {
-            var defaultInput = data.split(",")  
-   
-          console.log(defaultInput[0])
-          console.log(defaultInput[1])
+        }
+    
+        else {
+        var defaultInput = data.split(",")  
+
+        console.log(defaultInput[0])
+        console.log(defaultInput[1])
 
         type = defaultInput[0];
         inputParameter = defaultInput[1];
         switchType(type, inputParameter);
 
         }
-
     })
-
 }
