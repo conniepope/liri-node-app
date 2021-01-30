a187615494549ba30c9352eac31bd305ce74f20f
Connie Pope <conniepope77@gmail.com>
2021-01-29 20:52:15 -0500

    removed Spotify option

--- liri.js
+++ liri.js
@@ -1,10 +1,5 @@
 require("dotenv").config();
 
-var keys = require("./keys.js");
-
-var Spotify = require('node-spotify-api');
-var spotify = new Spotify(keys.spotify);
-
 var axios = require("axios");
 
 var fs = require("fs")
@@ -27,17 +22,11 @@ var switchType = function(type, inputParameter) {
         case "concert-this":
         concert(inputParameter);
         break;
-        case "spotify-this-song":
-        song(inputParameter);
-        break;
         case "movie-this":
         movie(inputParameter);
         break;
-        case "do-what-it-says":
-        says();
-        break;
         default: 
-        console.log("Invalid input. Please type one of the following options: \n  concert-this \n  spotify-this-song \n  movie-this \n  do-what-it-says")
+        console.log("Invalid input. Please type one of the following options: \n  'concert-this' or 'movie-this'")
     }
 }    
 switchType(type, inputParameter);
@@ -76,39 +65,6 @@ function concert() {
 }   
 // _____________________________________________________
 
-function song() {     
-
-    var song = inputParameter;
-    
-    if (process.argv[3] === undefined && song !== "I want it that way") {
-        song = "The Sign"
-    }
-
-    spotify                                   
-        .search({
-            type: "track",
-            query: song})
-        .then(function(response) {
-            
-            for (var i = 0; i < response.tracks.items.length; i++) {
-                var info = response.tracks.items[i]
-                console.log("------------ SONGS --------------");
-                fs.appendFileSync("log.txt", "------------ SONGS --------------\n");
-                console.log("Artist: " + info.artists[0].name);
-                fs.appendFileSync("log.txt", "Artist: " + info.artists[0].name + "\n");
-                console.log("Song: " + info.name);
-                fs.appendFileSync("log.txt", "Song: " + info.name + "\n");
-                console.log("Preview Link: " + info.preview_url); 
-                fs.appendFileSync("log.txt", "Preview Link: " + info.preview_url + "\n");  
-                console.log("Album: " + info.album.name); 
-                fs.appendFileSync("log.txt", "Album: " + info.album.name + "\n") 
-                console.log();  
-                fs.appendFileSync("log.txt", "\n")
-            }
-        })
-}       
-// _____________________________________________________
-
 function movie() {      
 
     var movieName = inputParameter;
@@ -143,7 +99,6 @@ function movie() {
         fs.appendFileSync("log.txt", "IMDB Rating: " + movie.imdbRating + "\n");
         console.log(movie.Ratings[1].Source + " Rating: " + movie.Ratings[1].Value);
         fs.appendFileSync("log.txt", movie.Ratings[1].Source + " Rating: " + movie.Ratings[1].Value + "\n");
-        // console.log("Rotten Tomatoes Rating: " + getRottenTomatoesRatingValue(movie))
         console.log();  
         fs.appendFileSync("log.txt", "\n")
         }
@@ -154,36 +109,4 @@ function movie() {
      });
 
 }
-    // //function to get proper Rotten Tomatoes Rating
-    // function getRottenTomatoesRating (data) {
-    //     return movie.Ratings.find(function (item) {
-    //     return item.Source === "Rotten Tomatoes";
-    //     });
-    // }
-    
-    // function getRottenTomatoesRatingValue (data) {
-    //     return getRottenTomatoesRating(data).Value;
-    // }
 
-// _____________________________________________________
-
-function says() {       
-
-    fs.readFile("random.txt", "utf8", function(err, data) {
-        if (err) {
-            return console.log(err);
-        }
-    
-        else {
-        var defaultInput = data.split(",")  
-
-        console.log(defaultInput[0])
-        console.log(defaultInput[1])
-
-        type = defaultInput[0];
-        inputParameter = defaultInput[1];
-        switchType(type, inputParameter);
-
-        }
-    })
-}
